1592e3ecd571a8cb503dba738ffaaea3c791705b
aynur <aykuli@ya.ru>
2023-11-09 14:40:09 +0700

    accordion

---
+++ src/components/Dashboard/components.tsx
@@ -0,0 +1,45 @@
+import { styled } from "@mui/material/styles"
+import {
+  Accordion as MuiAccordion,
+  AccordionProps,
+  AccordionSummary as MuiAccordionSummary,
+  AccordionSummaryProps,
+  AccordionDetails as MuiAccordionDetails,
+} from "@mui/material"
+import { ArrowForwardIosSharp } from "@mui/icons-material"
+
+export const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharp sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}))
+
+export const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}))
+
+export const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}))
