f674822882b8ead11663c09da5a91950226d3e69
aynur <aykuli@ya.ru>
2023-12-29 15:58:15 +0700

    form refactor

--- src/components/AddingModal/AddPersonModal.tsx
+++ src/components/AddingModal/AddPersonModal.tsx
@@ -25,49 +25,59 @@ const members = ["мама", "папа", "брат", "сестра", "дочь",
 const AddPersonModal = ({ open, setOpen }: AddPersonProps) => {
   const [firstName, setFirstName] = useState<string>("")
   const [lastName, setLastName] = useState<string>("")
+
   return (
     <Dialog fullScreen onClose={() => setOpen(false)} open={open}>
       <DialogTitle>Добавление человека</DialogTitle>
       <DialogContent dividers>
         <TextField
+          fullWidth
           required
           id="first-name"
           label="Имя"
           defaultValue={firstName}
         />
         <TextField
+          fullWidth
           margin="normal"
           id="last-name"
           label="Фамилия"
           defaultValue={lastName}
         />
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DemoContainer components={["DatePicker"]}>
-            <DatePicker label="Дата рождения" />
-          </DemoContainer>
-        </LocalizationProvider>
+        <div style={{ marginBottom: 16 }}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={["DatePicker"]}>
+              <DatePicker label="Дата рождения*" />
+            </DemoContainer>
+          </LocalizationProvider>
+        </div>
 
-        <Autocomplete
-          disablePortal
-          freeSolo
-          id="member"
-          options={members}
-          sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Член семьи" />}
-        />
+        <div style={{ marginBottom: 16 }}>
+          <Autocomplete
+            disablePortal
+            freeSolo
+            id="member"
+            options={members}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Член семьи" />
+            )}
+          />
+        </div>
         <FormControl>
           <FormLabel id="gender">Пол</FormLabel>
           <RadioGroup
             aria-labelledby="demo-radio-buttons-group-label"
             defaultValue="female"
-            name="radio-buttons-group"
+            name="gender-group"
+            style={{ display: "inline" }}
           >
-            <FormControlLabel
-              value="female"
-              control={<Radio />}
-              label="Female"
-            />
-            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            {[
+              { label: "Жен", value: "female" },
+              { label: "Муж", value: "male" },
+            ].map(({ label, value }) => (
+              <FormControlLabel {...{ value, label }} control={<Radio />} />
+            ))}
           </RadioGroup>
         </FormControl>
       </DialogContent>
